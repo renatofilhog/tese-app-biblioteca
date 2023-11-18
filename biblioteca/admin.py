13f67485c287e6c020343cc448377c2fc0f79a40
Renato Filho <frenato258@gmail.com>
2023-11-18 01:23:46 -0300

    CRUD on Django Admin for Artigos

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from biblioteca.models import Artigo
 # Register your models here.
+
+class ListArtigos(admin.ModelAdmin):
+    list_display = ('autor', 'titulo', 'data_publicacao','publicado')
+    list_filter = ('autor', 'titulo', 'publicado','data_publicacao')
+    search_fields = ('autor', 'titulo', 'publicado','data_publicacao')
+    list_display_links = ('autor', 'titulo')
+    list_editable = ('publicado',)
+
+
+admin.site.register(Artigo, ListArtigos)
--- setup/settings.py
+++ setup/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'biblioteca',
+    'biblioteca.apps.BibliotecaConfig',
 ]
 
 MIDDLEWARE = [
