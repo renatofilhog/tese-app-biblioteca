77b68517d5f210406b83e8abd6e07a62f1ced6e4
Renato Filho <frenato258@gmail.com>
2023-11-18 00:58:54 -0300

    Model de Artigo

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# para instalacao 
+- git clone
+- Criar ambiente virtual 'python -m venv .venv'
+- Ativar ambiente virtual 'source .venv/bin/activate'
+- Instalar dependencias 'pip install -r requirements.txt'
+- Criar arquivo .env e adicionar as variaveis de ambiente
+- Criar banco de dados 'python manage.py migrate'
+- Criar super usuario 'python manage.py createsuperuser'
+- Rodar o projeto 'python manage.py runserver'
+```
--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=100)
+    resumo = models.TextField()
+    autor = models.CharField(max_length=100)
+    palavras_chave = models.CharField(max_length=100)
+    arquivo = models.FileField(upload_to='artigos/', null=True, blank=True)
+    publicado = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
+    data_publicacao = models.DateField(null=True, blank=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.autor} - {self.titulo} - {self.data_publicacao}"
